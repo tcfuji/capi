0ed33543d36558c25b8b0d3614beaf75e85c8f21
tcfuji <tfturing@gmail.com>
2022-01-30 13:39:37 -0800

    fixed epsilon file bug

--- scripts/interface_adv.py
+++ scripts/interface_adv.py
@@ -67,4 +67,4 @@ if __name__ == "__main__":
     )
     torch.manual_seed(seed)
     trainer = Trainer(g, agent, 'adversarial', args.directory)
-    trainer.run(args.num_episodes, args.write_every, seed, args.num_items, args.epsilon)
+    trainer.run(args.num_episodes, args.write_every, seed, args.num_items, args.adv_epsilon)
